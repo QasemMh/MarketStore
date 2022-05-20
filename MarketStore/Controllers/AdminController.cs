effab24b69957815a9e8cf4b157bdc45d4d787ab
QasemMh <kasem200094@gmail.com>
2022-05-21 02:53:34 +0300

    Add Dashboards

---
+++ MarketStore/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
